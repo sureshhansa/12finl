b4db5cf5ad1696ddae8326cb7da778161c80d5b7
sureshhansa <68280941+sureshhansa@users.noreply.github.com>
2023-03-15 14:58:45 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,7 @@
-import type { NextPage } from 'next'
+import type { GetServerSidePropsContext, NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import type { GetServerSidePropsContext, NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
